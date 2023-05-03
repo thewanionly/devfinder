ecffe47d492ea69eac6784bf76d35bd6e011344b
thewanionly <pelloani@gmail.com>
2023-05-04 04:56:08 +0800

    refactor: add hex values of the colors as comments

--- src/styles/variables/colors.ts
+++ src/styles/variables/colors.ts
@@ -1,11 +1,11 @@
 export const COLORS = {
-  deepSkyBlue: 'hsl(212, 100%, 50%)',
-  greyishBlue: 'hsl(217, 20%, 51%)',
-  kashmirBlue: 'hsl(217, 35%, 45%)',
-  gunmetal: 'hsl(217, 21%, 21%)',
+  deepSkyBlue: 'hsl(212, 100%, 50%)', // #0079FF
+  greyishBlue: 'hsl(217, 20%, 51%)', // #697C9A
+  kashmirBlue: 'hsl(217, 35%, 45%)', // #4B6A9B
+  gunmetal: 'hsl(217, 21%, 21%)', //  #2B3442
   dark: 'hsla(220, 18%, 16%, 1)', // #222731
-  mirage: 'hsl(220, 40%, 13%)',
-  blueZodiac: 'hsl(222, 41%, 20%)',
-  zircon: 'hsl(227, 100%, 98%)',
-  white: 'hsl(0, 0%, 100%)',
+  mirage: 'hsl(220, 40%, 13%)', // #141D2F
+  blueZodiac: 'hsl(222, 41%, 20%)', // #1E2A47
+  zircon: 'hsl(227, 100%, 98%)', // #F6F8FF
+  white: 'hsl(0, 0%, 100%)', //  #FFFFFF or #FEFEFE
 } as const
